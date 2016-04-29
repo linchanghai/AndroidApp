651d4a090d540812182d8c505247198dd1b590f9
changhai.lin <changhai.lin@jago-ag.cn>
2016-04-29 16:54:02 +0800

    no message

---
+++ app/src/main/java/com/example/chalin/firstapplication/DisplayMessageActivity.java
@@ -0,0 +1,44 @@
+package com.example.chalin.firstapplication;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+    public final String TAG = "DisplayMessageActivity";
+
+    public EditText editText, editText2, editText3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+
+        String name = intent.getStringExtra("name");
+        Bundle bundle = intent.getBundleExtra("bundle");
+        Integer age = bundle.getInt("age");
+
+        textView.setText(message + "--" + name + "(" + age + ")");
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
+    }
+
+    public void calculate(View view) {
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        editText3 = (EditText) findViewById(R.id.editText3);
+    }
+}
---
+++ app/src/main/java/com/example/chalin/firstapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.chalin.firstapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.chalin.firstapplication.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra("name", "Chunk");
+        Bundle bundle = new Bundle();
+        bundle.putInt("age", 25);
+        intent.putExtra("bundle", bundle);
+        startActivity(intent);
+    }
+}
